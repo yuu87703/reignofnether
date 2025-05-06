71e42e755d7845ba466196db5ce56030edd968e9
SoLegendary <nick.tangson@gmail.com>
2025-05-06 22:39:13 +1000

    1.1.6 version

--- src/main/java/com/solegendary/reignofnether/ReignOfNether.java
+++ src/main/java/com/solegendary/reignofnether/ReignOfNether.java
@@ -45,7 +45,7 @@ import java.util.function.Supplier;
 public class ReignOfNether {
     public static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "reignofnether";
-    public static final String VERSION_STRING = "1.1.5c";
+    public static final String VERSION_STRING = "1.1.6";
 
     // Fields from ClientReset
     public static final Field handshakeField;
