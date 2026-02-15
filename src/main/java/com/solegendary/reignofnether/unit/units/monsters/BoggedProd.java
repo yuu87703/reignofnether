9a2c58e0daba7b47ebeadbc49402ec28ebb72087
SoLegendary <nick.tangson@gmail.com>
2026-02-15 19:48:04 +1100

    wraith fixes

--- src/main/java/com/solegendary/reignofnether/unit/units/monsters/BoggedProd.java
+++ src/main/java/com/solegendary/reignofnether/unit/units/monsters/BoggedProd.java
@@ -66,8 +66,6 @@ public class BoggedProd extends ProductionItem {
                 ResourceCosts.getFormattedPopAndTime(cost),
                 FormattedCharSequence.forward("", Style.EMPTY),
                 FormattedCharSequence.forward(I18n.get("units.monsters.reignofnether.bogged.tooltip1"), Style.EMPTY),
-                FormattedCharSequence.forward("", Style.EMPTY),
-                FormattedCharSequence.forward(I18n.get("units.monsters.reignofnether.bogged.tooltip1"), Style.EMPTY),
                 FormattedCharSequence.forward(I18n.get("units.monsters.reignofnether.bogged.tooltip2"), Style.EMPTY),
                 FormattedCharSequence.forward("", Style.EMPTY),
                 FormattedCharSequence.forward(I18n.get("units.monsters.reignofnether.bogged.tooltip3"), Style.EMPTY)
