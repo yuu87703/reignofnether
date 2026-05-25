f219021ea6a1835ca38fbf5cecbc1c9a26f7ca86
SoLegendary <nick.tangson@gmail.com>
2026-05-26 00:08:32 +1000

    sound fix

--- src/main/java/com/solegendary/reignofnether/sounds/SoundClientEvents.java
+++ src/main/java/com/solegendary/reignofnether/sounds/SoundClientEvents.java
@@ -1,16 +1,26 @@
 package com.solegendary.reignofnether.sounds;
 
+import com.solegendary.reignofnether.building.BuildingClientEvents;
+import com.solegendary.reignofnether.building.BuildingPlacement;
+import com.solegendary.reignofnether.building.BuildingUtils;
+import com.solegendary.reignofnether.minimap.MinimapClientEvents;
 import com.solegendary.reignofnether.registrars.SoundRegistrar;
 import com.solegendary.reignofnether.faction.Faction;
+import com.solegendary.reignofnether.unit.UnitClientEvents;
+import com.solegendary.reignofnether.util.MiscUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -33,6 +43,8 @@ public class SoundClientEvents {
     private static final int SOUND_DEDUPE_WINDOW_TICKS = 5;
     private static final float SOUND_VOLUME_DIMINISH_FACTOR = 0.7f; // each stack multiplies by this
     private static final int SOUND_MAX_STACKS = 10; // volume floors at diminish^maxStacks
+
+    public static final float SOUND_RANGE = 128; // all sounds have this max range
 
     @SubscribeEvent
     public static void onClientTick(TickEvent.ClientTickEvent evt) {
@@ -120,7 +132,13 @@ public class SoundClientEvents {
 
         times.add(currentTick);
 
-        level.playLocalSound(bp.getX(), bp.getY(), bp.getZ(), soundEvent, SoundSource.NEUTRAL, diminishedVolume, 1.0F, false);
+        Vec3 listenerPos = getOrthoviewSoundPos();
+
+        float dist = (float) Math.sqrt(listenerPos.distanceToSqr(bp.getCenter()));
+        if (dist > SOUND_RANGE) return;
+        float attenuatedVolume = dist <= (SOUND_RANGE / 4) ? diminishedVolume : diminishedVolume * (1.0f - (dist - (SOUND_RANGE / 4)) / (((SOUND_RANGE * 3) / 4)));
+
+        level.playLocalSound(bp.getX(), bp.getY(), bp.getZ(), soundEvent, SoundSource.NEUTRAL, attenuatedVolume, 1.0F, false);
     }
 
     public static void playFadeableSoundAtPos(SoundAction soundAction, BlockPos bp, float volume, int id, int tickDuration) {
@@ -150,6 +168,53 @@ public class SoundClientEvents {
         if (MC.player != null && MC.player.getName().getString().equals(playerName)) {
             MC.player.playSound(SOUND_MAP.get(soundAction), volume, 1.0f);
         }
+    }
+
+    // calculates the location of where local world sounds should play for an orthoview player based on the selected
+    // units and/or buildings that are currently on the screen
+    // eg. if the player has selected a unit at pos (4,6) but their player entity is at (34,52) and a sound plays at
+    // (10,10), the new sound position should instead be (10,10) - (4,6) + (34,52) = (40,56)
+    // if nothing is selected, then default to the centre of the screen
+    public static Vec3 getOrthoviewSoundPos() {
+        return getOrthoviewSoundPos(null);
+    }
+
+    public static Vec3 getOrthoviewSoundPos(@Nullable Vec3 originalPos) {
+        Player player = MC.player;
+        if (player == null)
+            return originalPos;
+
+        ArrayList<Vec3> posList = new ArrayList<>();
+
+        for (LivingEntity entity : UnitClientEvents.getSelectedUnits())
+            posList.add(entity.getEyePosition());
+        for (BuildingPlacement building : BuildingClientEvents.getSelectedBuildings()) {
+            BlockPos bp = BuildingUtils.getCentrePos(building.getBlocks());
+            posList.add(new Vec3(bp.getX(), bp.getY(), bp.getZ()));
+        }
+        // remove any positions that aren't on the screen
+        List<Vec3> posListOnScreen = new ArrayList<>();
+        for (Vec3 vec3 : posList) {
+            if (MinimapClientEvents.isWorldXZinsideMap((int) vec3.x, (int) vec3.z)) {
+                posListOnScreen.add(vec3);
+            }
+        }
+
+        // calculate the average position
+        Vec3 newPos = new Vec3(0,0,0);
+        if (!posListOnScreen.isEmpty()) {
+            for (Vec3 pos : posListOnScreen)
+                newPos = newPos.add(pos);
+            double m = 1D/posListOnScreen.size();
+            newPos = newPos.multiply(m, m, m);
+        } else {
+            // do a similar kind of calculation to get the pos at the centre of the screen as in CursorClientEvents
+            newPos = MiscUtil.getOrthoviewCentreWorldPos(MC);
+        }
+        // get the position for the sound as though the player was at position newPos
+        Vec3 diffOriginalToNew = originalPos != null ?
+                originalPos.add(newPos.multiply(-1,-1,-1)) : new Vec3(0,0,0);
+        return new Vec3(player.getX(), player.getY(), player.getZ()).add(diffOriginalToNew);
     }
 
     // sounds which shouldn't follow the ClientLevelMixin rules of being changed to the location of what is selected
